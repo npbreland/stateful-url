e48137e49ae72502a67dbbd1288fac4f4d282861
Nick Breland <npbreland@tutanota.com>
2022-05-26 19:07:46 +0100

    Add file

---
+++ stateful-url.js
@@ -0,0 +1,19 @@
+const statefulUrl = {
+
+  updateParam: (name, value) => {
+    const params = new URLSearchParams(location.search);
+    params.set(name, value);
+    const newURL = `${window.location.pathname}?${params}`;
+    window.history.replaceState({}, '', newURL);
+  },
+
+  getParam: (name) => {
+    const params = new URLSearchParams(location.search);
+    return params.get(name);
+  },
+
+  clearParams: () => {
+    window.history.replaceState({}, '', window.location.pathname);
+  }
+
+};
